642da29642c39d4c2cd09092d9b4665d51d60e7e
Sanvi028 <ingle.sanvi20@gmail.com>
2026-05-30 16:12:37 +0530

    setup express server with mongoDB connection and env config

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://SanviProj1:%23win123@cluster0.lqmucow.mongodb.net/interviewDB?retryWrites=true&w=majority"
-)
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log(err));
+//database connection
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Server running");
 });
 
-app.listen(7000, () => console.log("Server started on port 7000"));
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
